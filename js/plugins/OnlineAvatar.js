c9d1c19623f4a457efa6bd0a447d463891ef2209
CactusMan <unofficialsquaregames@gmail.com>
2022-05-10 22:24:29 +0900

    修正

--- js/plugins/OnlineAvatar.js
+++ js/plugins/OnlineAvatar.js
@@ -689,8 +689,8 @@ function Game_Avatar() {
                 $gameSystem._phaseState = 12;//事後処理
                 break;
             case 12: //事後処理
-                if($gameSwitches.value(20)) this.endTurn();
                 $gameSystem.syncVariable();
+                if($gameSwitches.value(20)) this.endTurn();
                 break;
         }
     };
