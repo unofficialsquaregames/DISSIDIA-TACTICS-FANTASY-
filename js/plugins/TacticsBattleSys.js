ee530407629201025cd8f603527cabf43af5d160
CactusMan <unofficialsquaregames@gmail.com>
2022-06-05 18:40:56 +0900

    修正

--- js/plugins/TacticsBattleSys.js
+++ js/plugins/TacticsBattleSys.js
@@ -1612,7 +1612,7 @@ Imported.TacticsBattleSys = true;
     //パラダイムシフトやベロシティチェンジ、ラウンドアサルトなどで行動順を繰り上げてもらう
     Game_BattlerBase.prototype.wtTurnAdvance = function () {
         if ($gameSwitches.value(15)) {
-            if (gameSystem.isSyncTurn()) {
+            if ($gameSystem.isSyncTurn()) {
                 $gameSystem.syncWtList();
             } else {
                 this._wt = this.wtTurn();
