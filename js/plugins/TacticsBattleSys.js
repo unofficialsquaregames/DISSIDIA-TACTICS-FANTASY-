3f432c2a797b00b08883651cae28cb6ac349220b
CactusMan <unofficialsquaregames@gmail.com>
2022-06-06 14:49:40 +0900

    修正

--- js/plugins/TacticsBattleSys.js
+++ js/plugins/TacticsBattleSys.js
@@ -3813,12 +3813,12 @@ Imported.TacticsBattleSys = true;
             actor.addState(this.event().id, 1);
             this.checkDead();
         }
-        if ($gameSwitches.value(15)) {
-            if ($gameSystem.isSyncTurn()) $gameSystem.syncState();
-            else actor.updateStateTurns(); //バフ期間1act減少
-        } else {
+        //if ($gameSwitches.value(15)) {
+        //    if ($gameSystem.isSyncTurn()) $gameSystem.syncState();
+        //    else actor.updateStateTurns(); //バフ期間1act減少
+        //} else {
             actor.updateStateTurns(); //バフ期間1act減少
-        }
+        //}
     };
 
     //行動負荷バフチェック
